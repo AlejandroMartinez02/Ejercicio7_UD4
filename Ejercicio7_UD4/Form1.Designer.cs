5f12dce0157224e50e0892fc65d4f0e6d4aeac25
Alejandro Martinez <martinezmorilloalejandro@gmail.com>
2022-03-16 19:11:40 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio7_UD4/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Ejercicio7_UD4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Joe_saldo = new System.Windows.Forms.Label();
+            this.Bob_saldo = new System.Windows.Forms.Label();
+            this.banca = new System.Windows.Forms.Label();
+            this.aJoe10e = new System.Windows.Forms.Button();
+            this.deBob5e = new System.Windows.Forms.Button();
+            this.JoeABob = new System.Windows.Forms.Button();
+            this.BobAJoe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Joe_saldo
+            // 
+            this.Joe_saldo.AutoSize = true;
+            this.Joe_saldo.Location = new System.Drawing.Point(110, 131);
+            this.Joe_saldo.Name = "Joe_saldo";
+            this.Joe_saldo.Size = new System.Drawing.Size(109, 20);
+            this.Joe_saldo.TabIndex = 0;
+            this.Joe_saldo.Text = "Joe tiene 50eur";
+            // 
+            // Bob_saldo
+            // 
+            this.Bob_saldo.AutoSize = true;
+            this.Bob_saldo.Location = new System.Drawing.Point(110, 72);
+            this.Bob_saldo.Name = "Bob_saldo";
+            this.Bob_saldo.Size = new System.Drawing.Size(122, 20);
+            this.Bob_saldo.TabIndex = 1;
+            this.Bob_saldo.Text = "Bob tiene 100eur";
+            // 
+            // banca
+            // 
+            this.banca.AutoSize = true;
+            this.banca.ForeColor = System.Drawing.Color.Red;
+            this.banca.Location = new System.Drawing.Point(110, 190);
+            this.banca.Name = "banca";
+            this.banca.Size = new System.Drawing.Size(154, 20);
+            this.banca.TabIndex = 2;
+            this.banca.Text = "La banca tiene 100eur";
+            // 
+            // aJoe10e
+            // 
+            this.aJoe10e.Location = new System.Drawing.Point(265, 251);
+            this.aJoe10e.Name = "aJoe10e";
+            this.aJoe10e.Size = new System.Drawing.Size(94, 71);
+            this.aJoe10e.TabIndex = 3;
+            this.aJoe10e.Text = "Dar 10e E Joe";
+            this.aJoe10e.UseVisualStyleBackColor = true;
+            this.aJoe10e.Click += new System.EventHandler(this.aJoe10e_Click);
+            // 
+            // deBob5e
+            // 
+            this.deBob5e.Location = new System.Drawing.Point(442, 251);
+            this.deBob5e.Name = "deBob5e";
+            this.deBob5e.Size = new System.Drawing.Size(94, 71);
+            this.deBob5e.TabIndex = 4;
+            this.deBob5e.Text = "Recibir 5e de Bob";
+            this.deBob5e.UseVisualStyleBackColor = true;
+            this.deBob5e.Click += new System.EventHandler(this.aBob5e_Click);
+            // 
+            // JoeABob
+            // 
+            this.JoeABob.Location = new System.Drawing.Point(265, 359);
+            this.JoeABob.Name = "JoeABob";
+            this.JoeABob.Size = new System.Drawing.Size(94, 71);
+            this.JoeABob.TabIndex = 5;
+            this.JoeABob.Text = "Joe da 10E a Bob";
+            this.JoeABob.UseVisualStyleBackColor = true;
+            this.JoeABob.Click += new System.EventHandler(this.JoeABob_Click);
+            // 
+            // BobAJoe
+            // 
+            this.BobAJoe.Location = new System.Drawing.Point(442, 359);
+            this.BobAJoe.Name = "BobAJoe";
+            this.BobAJoe.Size = new System.Drawing.Size(94, 71);
+            this.BobAJoe.TabIndex = 6;
+            this.BobAJoe.Text = "Bob da 5e a Joe";
+            this.BobAJoe.UseVisualStyleBackColor = true;
+            this.BobAJoe.Click += new System.EventHandler(this.BobAJoe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BobAJoe);
+            this.Controls.Add(this.JoeABob);
+            this.Controls.Add(this.deBob5e);
+            this.Controls.Add(this.aJoe10e);
+            this.Controls.Add(this.banca);
+            this.Controls.Add(this.Bob_saldo);
+            this.Controls.Add(this.Joe_saldo);
+            this.Name = "Form1";
+            this.Text = "Fun with Joe and Bob";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Joe_saldo;
+        private System.Windows.Forms.Label Bob_saldo;
+        private System.Windows.Forms.Label banca;
+        private System.Windows.Forms.Button aJoe10e;
+        private System.Windows.Forms.Button deBob5e;
+        private System.Windows.Forms.Button JoeABob;
+        private System.Windows.Forms.Button BobAJoe;
+    }
+}
+
---
+++ Ejercicio7_UD4/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio7_UD4
+{
+    public partial class Form1 : Form
+    {
+        Guy Bob = new Guy("Bob", 100);
+        Guy Joe = new Guy("Joe", 50);
+        int banco = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Guy
+        {
+            public String nombre;
+            public int saldo;
+
+
+            public void recibirCash()
+            {
+                saldo = saldo + 10;
+               
+            }
+
+
+            public void darCash()
+            {
+                saldo = saldo - 5;
+            }
+
+            public Guy()
+            {
+
+            }
+
+            public Guy(String name, int cash)
+            {
+                saldo = cash;
+                nombre = name;
+            }
+           
+
+        }
+
+        public void actualizaForm()
+        {
+            Bob_saldo.Text = "Bob tiene " + Bob.saldo + "eur";
+            Joe_saldo.Text = "Joe tiene " + Joe.saldo + "eur";
+        }
+
+        private void aJoe10e_Click(object sender, EventArgs e)
+        {
+           
+            if(banco == 0)
+            {
+                MessageBox.Show("Estamos en BANCA ROTA");
+            }
+            else
+            {
+                
+                Joe.recibirCash();
+                banco -= 10;
+                actualizaForm();
+                banca.Text = "La banca tiene " + banco + "eur";
+            }
+            
+            
+
+        }
+
+        private void BobAJoe_Click(object sender, EventArgs e)
+        {
+            if (Bob.saldo == 0)
+            {
+                MessageBox.Show("No puedo darle más dinero a Joe");
+            }
+            else
+            {
+                Joe.saldo += 5;
+                Bob.saldo -= 5;
+                actualizaForm();
+            }
+        }
+
+        private void aBob5e_Click(object sender, EventArgs e)
+        {
+
+
+
+            if(Bob.saldo == 0)
+            {
+                MessageBox.Show("No tengo saldo suficiente como para darte 5");
+            }
+            else
+            {
+                Bob.darCash();
+                banco += 5;
+                banca.Text = "La banca tiene " + banco + "eur";
+                actualizaForm();
+
+            }
+
+
+        }
+        private void JoeABob_Click(object sender, EventArgs e)
+        {
+            
+
+            if(Joe.saldo == 0)
+            {
+                MessageBox.Show("No puedo darle más dinero a Bob");
+            }
+            else
+            {
+                Joe.saldo -= 10;
+                Bob.saldo += 10;
+                actualizaForm();
+            }
+
+ 
+         
+        }
+    }
+
+  
+}
