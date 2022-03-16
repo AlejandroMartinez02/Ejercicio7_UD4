f51cc1fda7db1dc99cba1e884a93ca3fdaa6de56
Hermann Hevesi <hermann.ffcv@gmail.com>
2022-03-16 19:33:19 +0100

    Hermann Commit

--- Ejercicio7_UD4/Form1.Designer.cs
+++ Ejercicio7_UD4/Form1.Designer.cs
@@ -41,18 +41,18 @@ namespace Ejercicio7_UD4
             // Joe_saldo
             // 
             this.Joe_saldo.AutoSize = true;
-            this.Joe_saldo.Location = new System.Drawing.Point(110, 131);
+            this.Joe_saldo.Location = new System.Drawing.Point(96, 98);
             this.Joe_saldo.Name = "Joe_saldo";
-            this.Joe_saldo.Size = new System.Drawing.Size(109, 20);
+            this.Joe_saldo.Size = new System.Drawing.Size(85, 15);
             this.Joe_saldo.TabIndex = 0;
             this.Joe_saldo.Text = "Joe tiene 50eur";
             // 
             // Bob_saldo
             // 
             this.Bob_saldo.AutoSize = true;
-            this.Bob_saldo.Location = new System.Drawing.Point(110, 72);
+            this.Bob_saldo.Location = new System.Drawing.Point(96, 54);
             this.Bob_saldo.Name = "Bob_saldo";
-            this.Bob_saldo.Size = new System.Drawing.Size(122, 20);
+            this.Bob_saldo.Size = new System.Drawing.Size(95, 15);
             this.Bob_saldo.TabIndex = 1;
             this.Bob_saldo.Text = "Bob tiene 100eur";
             // 
@@ -60,17 +60,18 @@ namespace Ejercicio7_UD4
             // 
             this.banca.AutoSize = true;
             this.banca.ForeColor = System.Drawing.Color.Red;
-            this.banca.Location = new System.Drawing.Point(110, 190);
+            this.banca.Location = new System.Drawing.Point(96, 142);
             this.banca.Name = "banca";
-            this.banca.Size = new System.Drawing.Size(154, 20);
+            this.banca.Size = new System.Drawing.Size(121, 15);
             this.banca.TabIndex = 2;
             this.banca.Text = "La banca tiene 100eur";
             // 
             // aJoe10e
             // 
-            this.aJoe10e.Location = new System.Drawing.Point(265, 251);
+            this.aJoe10e.Location = new System.Drawing.Point(232, 188);
+            this.aJoe10e.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.aJoe10e.Name = "aJoe10e";
-            this.aJoe10e.Size = new System.Drawing.Size(94, 71);
+            this.aJoe10e.Size = new System.Drawing.Size(82, 53);
             this.aJoe10e.TabIndex = 3;
             this.aJoe10e.Text = "Dar 10e E Joe";
             this.aJoe10e.UseVisualStyleBackColor = true;
@@ -78,9 +79,10 @@ namespace Ejercicio7_UD4
             // 
             // deBob5e
             // 
-            this.deBob5e.Location = new System.Drawing.Point(442, 251);
+            this.deBob5e.Location = new System.Drawing.Point(387, 188);
+            this.deBob5e.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.deBob5e.Name = "deBob5e";
-            this.deBob5e.Size = new System.Drawing.Size(94, 71);
+            this.deBob5e.Size = new System.Drawing.Size(82, 53);
             this.deBob5e.TabIndex = 4;
             this.deBob5e.Text = "Recibir 5e de Bob";
             this.deBob5e.UseVisualStyleBackColor = true;
@@ -88,9 +90,10 @@ namespace Ejercicio7_UD4
             // 
             // JoeABob
             // 
-            this.JoeABob.Location = new System.Drawing.Point(265, 359);
+            this.JoeABob.Location = new System.Drawing.Point(232, 269);
+            this.JoeABob.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.JoeABob.Name = "JoeABob";
-            this.JoeABob.Size = new System.Drawing.Size(94, 71);
+            this.JoeABob.Size = new System.Drawing.Size(82, 53);
             this.JoeABob.TabIndex = 5;
             this.JoeABob.Text = "Joe da 10E a Bob";
             this.JoeABob.UseVisualStyleBackColor = true;
@@ -98,9 +101,10 @@ namespace Ejercicio7_UD4
             // 
             // BobAJoe
             // 
-            this.BobAJoe.Location = new System.Drawing.Point(442, 359);
+            this.BobAJoe.Location = new System.Drawing.Point(387, 269);
+            this.BobAJoe.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BobAJoe.Name = "BobAJoe";
-            this.BobAJoe.Size = new System.Drawing.Size(94, 71);
+            this.BobAJoe.Size = new System.Drawing.Size(82, 53);
             this.BobAJoe.TabIndex = 6;
             this.BobAJoe.Text = "Bob da 5e a Joe";
             this.BobAJoe.UseVisualStyleBackColor = true;
@@ -108,9 +112,10 @@ namespace Ejercicio7_UD4
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.BobAJoe);
             this.Controls.Add(this.JoeABob);
             this.Controls.Add(this.deBob5e);
@@ -118,6 +123,7 @@ namespace Ejercicio7_UD4
             this.Controls.Add(this.banca);
             this.Controls.Add(this.Bob_saldo);
             this.Controls.Add(this.Joe_saldo);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Fun with Joe and Bob";
             this.ResumeLayout(false);
